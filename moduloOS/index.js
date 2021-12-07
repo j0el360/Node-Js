91cf29c24ef1a027c4a488c450181c559dd8b87b
j0el360 <joelguerra360@gmail.com>
2021-12-07 01:41:15 -0500

    Modulo OS funcionamiento

---
+++ moduloOS/index.js
@@ -0,0 +1,13 @@
+const os = require('os');
+
+//Sistema operativo
+console.log(os.platform());
+
+//Version de windows
+console.log(os.release());
+
+//Memoria libre
+console.log(os.freemem());
+
+//Memoria Total
+console.log(os.totalmem());17125707776
